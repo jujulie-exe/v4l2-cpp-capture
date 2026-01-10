6f17c9e76e160d2ffccd86f1f96b82686164940c
jujulie-exe <julie.exe.franco@gmail.com>
2026-01-10 18:19:36 +0100

    ADD: compile okey but continue working su implementazione

--- include/Camera.hpp
+++ include/Camera.hpp
@@ -1,40 +1,58 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+#include <string>
+#include <vector>
+#include <exception>
 #include <unistd.h>
 #include <sys/mman.h> 
+
 //https://docs.kernel.org/userspace-api/media/v4l/func-mmap.html
 //https://www.kernel.org/doc/html/latest/userspace-api/media/v4l/common.html
 //https://www.kernel.org/doc/html/v4.9/media/uapi/v4l/buffer.html
 //https://www.kernel.org/doc/html/v4.9/media/uapi/v4l/vidioc-create-bufs.html#vidioc-create-bufs
+
 class Camera
 {
    public:
        /*♡♡♡♡♡♡♡♡♡♡♡CTOR♡♡♡♡♡♡♡♡♡♡♡♡♡*/
-       Camera(const std::string& device, size_t lght; size_t wdt);  //cannon
-       Camera(Camera const & src);   //Cannon
+       Camera(const std::string& pathDevice, size_t lght, size_t wdt);  //cannon
+       Camera(Camera const & src) = delete ;   //Cannon
  
        /*♡♡♡♡♡♡♡♡♡♡♡GETTER♡♡♡♡♡♡♡♡♡♡♡♡♡*/
-	   void getNameCamera(void) const;
-	   std::vector<std::string> > getFormatType(void) const;
+	   const std::string getNameCamera(void) const;
+	   const std::vector<std::string>  getFormatType(void) const;
  
        /*♡♡♡♡♡♡♡♡♡♡♡FT♡♡♡♡♡♡♡♡♡♡♡♡♡*/
-	   bool setParameters(int falgs);
+	   bool setParameters(int flags);
 	   bool takeAFrame( void );
  
        /*♡♡♡♡♡♡♡♡♡♡♡OPERATOR♡♡♡♡♡♡♡♡♡♡♡♡♡*/
-       Camera& operator=(Camera const & rsh);    //Cannon
+       Camera& operator=(Camera const & rsh) = delete;    //Cannon
  
        /*♡♡♡♡♡♡♡♡♡♡♡DTOR♡♡♡♡♡♡♡♡♡♡♡♡♡*/
        virtual ~Camera(); //Cannon
+	   class ErrorOpen : public std::exception
+		{
+			public:
+				const char * what () const noexcept override { return "Failed to open FD path"; }
+		};
+
+	   class IsNotACamera : public std::exception
+		{
+			public:
+				const char * what () const noexcept override { return "Is not a camera"; }
+		};
+
   private:
 	   bool ft_ioctl(const int fd, const int flags, const void *args) const;
-	   bool initABuffer( void )
+	   bool initABuffer( void );
 	   int _fd;
+	   size_t _buffer_lght;
+	   size_t _buffer_wdt;
 	   std::string _device;
-       Camera();  //cannon
+       void* _buffer;
 
 };
 // std::ostream &operator<<(std::ostream &o, const Camera &rhs);
 #endif // CAMERA_H
-
--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,53 +1,83 @@
 #include "../include/Camera.hpp"
+#include <iostream>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/videodev2.h>
+#include <cstring> // For memset
 
        /*♡♡♡♡♡♡♡♡♡♡♡CTOR♡♡♡♡♡♡♡♡♡♡♡♡♡*/
-Camera::Camera()
+Camera::Camera(const std::string& pathDevice, size_t lght, size_t wdt)
+: _fd(-1), _buffer_lght(lght), _buffer_wdt(wdt), _device(pathDevice), _buffer(NULL)
 {
-	fd = open(device, O_NONBLOCK | O_RDWR);
-	if (fd > 0)
-		throw ErrorOpen;
-	struct v4l2_capability cap = {0};
-	ioctl(fd, VIDIOC_QUERYCAP, &cap);
+	this->_fd = open(_device.c_str(), O_NONBLOCK | O_RDWR);
+	if (_fd < 0)
+		throw ErrorOpen();
+	struct v4l2_capability cap;
+    memset(&cap, 0, sizeof(cap));
+	if (ioctl(_fd, VIDIOC_QUERYCAP, &cap) < 0)
+    {
+        // Handle ioctl error if needed, or just let the capability check fail
+    }
 	if (!(cap.capabilities & V4L2_CAP_VIDEO_CAPTURE)) {
 		throw IsNotACamera();
 	}
     // ctor
 }
 
-Camera::Camera(Camera const & src)
-{
-    std::cout << "Copy constructor called" << std::endl;
-    //*this = src;
-}
-
        /*♡♡♡♡♡♡♡♡♡♡♡GETTER♡♡♡♡♡♡♡♡♡♡♡♡♡*/
+const std::string Camera::getNameCamera() const {
+	return "";
+}
+const std::vector<std::string> Camera::getFormatType() const {
+	return {""};
+}
  
        /*♡♡♡♡♡♡♡♡♡♡♡FT♡♡♡♡♡♡♡♡♡♡♡♡♡*/
-bool	Camera::init(void)
+
+bool	Camera::initABuffer(void)
 {
-}
- 
-       /*♡♡♡♡♡♡♡♡♡♡♡OPERATOR♡♡♡♡♡♡♡♡♡♡♡♡♡*/
-Camera &Camera::operator=( Camera const &rhs)
-{
-    std::cout << "Copy assignment operator called" << std::endl;
-    if (this != &rhs)
-    {
-        // this->_n = rhs.getValue();
-    }
-    return *this;
+        struct v4l2_buffer buf;
+        memset(&buf, 0, sizeof(buf));
+        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        buf.memory = V4L2_MEMORY_MMAP;
+        buf.index = 0;
+        if (ioctl(_fd, VIDIOC_QUERYBUF, &buf) < 0) {
+            return false;
+        }
+        _buffer = mmap(NULL, buf.length, PROT_READ | PROT_WRITE, MAP_SHARED, _fd, buf.m.offset);
+        if (_buffer == MAP_FAILED) {
+            return false;
+        }
+        return true;
 }
 
+bool	Camera::takeAFrame( void )
+{ 
+
+	return false;
+}
+
+bool	Camera::setParameters( int flags )
+{
+    (void)flags; // Unused for now
+	return false;
+}
+
+bool Camera::ft_ioctl(const int fd, const int flags, const void *args) const
+{
+    // Wrapper for ioctl if needed, or implement logic
+    // For now just a placeholder as it was declared in header
+    if (ioctl(fd, flags, args) < 0)
+        return false;
+    return true;
+}
+ 
        /*♡♡♡♡♡♡♡♡♡♡♡DTOR♡♡♡♡♡♡♡♡♡♡♡♡♡*/
 Camera::~Camera()
 {
     std::cout << "Destructor called" << std::endl;
+    if (_fd > 0)
+        close(_fd);
+    // TODO: munmap _buffer if it was mapped
     // dtor
 }
-
-/* std::ostream &operator<<(std::ostream &o, const Camera &rhs)
-{
-   o << rhs.getName() << std::endl;
-   return o;
-} */
-
