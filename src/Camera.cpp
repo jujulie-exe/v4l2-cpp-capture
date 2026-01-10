7bdc5097442dacd85dec3f5b31e74c39807f073f
jujulie-exe <julie.exe.franco@gmail.com>
2026-01-10 23:52:48 +0100

    ADD: calsse per gestione v4l2 quasi completa manca il salvataggio del buffer e in un file path

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -7,7 +7,7 @@
 
        /*♡♡♡♡♡♡♡♡♡♡♡CTOR♡♡♡♡♡♡♡♡♡♡♡♡♡*/
 Camera::Camera(const std::string& pathDevice, size_t lght, size_t wdt)
-: _fd(-1), _buffer_lght(lght), _buffer_wdt(wdt), _device(pathDevice), _buffer(NULL)
+: _fd(-1), _fmt_lght(lght), _fmt_wdt(wdt), _device(pathDevice), _buffer(NULL)
 {
 	this->_fd = open(_device.c_str(), O_NONBLOCK | O_RDWR);
 	if (_fd < 0)
@@ -21,6 +21,7 @@ Camera::Camera(const std::string& pathDevice, size_t lght, size_t wdt)
 	if (!(cap.capabilities & V4L2_CAP_VIDEO_CAPTURE)) {
 		throw IsNotACamera();
 	}
+	_SetFormat();
     // ctor
 }
 
@@ -33,28 +34,118 @@ const std::vector<std::string> Camera::getFormatType() const {
 }
  
        /*♡♡♡♡♡♡♡♡♡♡♡FT♡♡♡♡♡♡♡♡♡♡♡♡♡*/
-
-bool	Camera::initABuffer(void)
-{
+int		Camera::_SetFormat( void ){
+		struct v4l2_format fmt = {0};
+		fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+		fmt.fmt.pix.width = _fmt_wdt;
+		fmt.fmt.pix.height = _fmt_lght;
+		//TODO verificare il fromato corretto 
+		fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_JPEG ;
+		fmt.fmt.pix.field = V4L2_FIELD_NONE;
+		if (ioctl(this->_fd, VIDIOC_S_FMT , &fmt) < 0){
+			return ERROR_SET_FMT;
+		}
+		return OK;
+}
+int		Camera::_reqBuffer(void){
+		struct v4l2_requestbuffers req = {0};
+		req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+		req.count = NUMBER_REQ_BUFFER;
+		req.memory = V4L2_MEMORY_MMAP;
+		if (ioctl(this->_fd,  VIDIOC_REQBUFS, &req) < 0){
+			return ERROR_REQ_BUFFER;
+		}
+		_buffer.resize(req.count);
+		_buffer_size.resize(req.count);
+		return OK;
+}
+int		Camera::_mmapBuffer(int index){
         struct v4l2_buffer buf;
         memset(&buf, 0, sizeof(buf));
         buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
         buf.memory = V4L2_MEMORY_MMAP;
-        buf.index = 0;
+        buf.index = index;
         if (ioctl(_fd, VIDIOC_QUERYBUF, &buf) < 0) {
-            return false;
+            return ERROR_QBUF;
         }
-        _buffer = mmap(NULL, buf.length, PROT_READ | PROT_WRITE, MAP_SHARED, _fd, buf.m.offset);
-        if (_buffer == MAP_FAILED) {
-            return false;
+		_buffer_size[index] = buf.length; 
+        _buffer[index] = mmap(NULL, buf.length, PROT_READ | PROT_WRITE, MAP_SHARED, _fd, buf.m.offset);
+        if (_buffer[index] == MAP_FAILED) {
+            return MAP_FAILED;
+		}
+        if (ioctl(_fd, VIDIOC_QBUF, &buf) < 0) {
+            return ERROR_QBUF;
         }
-        return true;
+}
+       /* struct epoll_event {
+           uint32_t      events;
+           epoll_data_t  data;    
+       };
+
+       union epoll_data {
+           void     *ptr;
+           int       fd;
+           uint32_t  u32;
+           uint64_t  u64;
+       }; */
+
+int		Camera::_epollStart( void ) {
+	this->_epoll_fd = epoll_create(0);
+	if (this->_epoll_fd < 0){
+		return ERROR_EPOLL_CREATE;
+	}
+	struct epoll_event ev = {0};
+	ev.events = EPOLLIN;
+	ev.data.fd = this->_fd;
+	if ( epoll_ctl(this->_epoll_fd, EPOLL_CTL_ADD, this->fd, &ev) < 0){
+		return ERROR_CTL_EPOLL;
+	}
+	return OK;
+}
+int		Camera::initV4L2(void)
+{
+	_reqBuffer();
+	for (size_t i = 0; i < _buffer.size(), ++i)
+	{
+		_mmapBuffer(i);
+	}
+	_epollStart();
+	enum v4l2_buf_type	type;
+	type.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+	if (ioctl(_fd,  VIDIOC_STREAMON , &type) < 0) {
+		return ERROR_QBUF;
+    }
+	_epollStart();
+	return OK;
 }
 
-bool	Camera::takeAFrame( void )
+int	Camera::takeAFrame( void )
 { 
+	struct epoll_event ev[1];
+	int ntfcs = -1;
 
-	return false;
+	ntfcs = epoll_wait(this->_epoll_fd, ev, 1, WATIN_TIME_MAX);
+
+	if (ntfcs < 0){
+		return 
+	}
+	if (ntfcs == 0)
+	{
+		return
+	}
+	struct v4l2_buffer buf = {0};
+	buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+	buf.memory = V4L2_MEMORY_MMAP;
+	// toglie dalla coda
+	if (ioctl(_fd,  VIDIOC_DQBUF , &buf) < 0) {
+		return ERROR_DQBUF;
+    }
+	// salvo in un vector con le info del buffer
+	// rimette in coda
+	if (ioctl(_fd,  VIDIOC_QBUF , &buf) < 0) {
+		return ERROR_QBUF;
+    }
+	return OK;
 }
 
 bool	Camera::setParameters( int flags )
